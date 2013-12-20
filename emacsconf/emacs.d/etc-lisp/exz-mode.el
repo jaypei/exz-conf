2a6140ebf21c9cb0bf125b457515760b736bca53
zhen.pei <zhen.pei@qunar.com>
2013-12-20 14:43:28 +0800

    add python-mode

--- emacsconf/emacs.d/etc-lisp/exz-mode.el
+++ emacsconf/emacs.d/etc-lisp/exz-mode.el
@@ -143,5 +143,27 @@
               (t "user"))))
 (setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)
 
+;; python-mode
+(setq py-install-directory "~/.emacs.d/site-lisp/python-mode")
+(exz-add-search-path "site-lisp/python-mode")
+(require 'python-mode)
+
+; use IPython
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+; use the wx backend, for both mayavi and matplotlib
+(setq py-python-command-args
+        '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq py-force-py-shell-name-p t)
+
+; switch to the interpreter after executing code
+(setq py-shell-switch-buffers-on-execute-p t)
+(setq py-switch-buffers-on-execute-p t)
+; don't split windows
+(setq py-split-windows-on-execute-p nil)
+; try to automagically figure out indentation
+(setq py-smart-indentation t)
+
+
 ;;; exz-mode.el ends here
 (provide 'exz-mode)
