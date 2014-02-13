433104a1803f0b0e03260e11e7da49f50abf99d6
jaypei <jaypei97159@gmail.com>
2014-02-14 01:12:51 +0800

    add exz-mail

---
+++ emacsconf/emacs.d/etc-lisp/exz-mail.el
@@ -0,0 +1,17 @@
+;;; Package --- exz-mail
+;;; Commentary:
+
+;;; Code:
+
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials '(("smtp.gmail.com" 587
+				   "jaypei97159@gmail.com" nil))
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+
+
+;;; exz-mail.el ends here
+(provide 'exz-mail)
