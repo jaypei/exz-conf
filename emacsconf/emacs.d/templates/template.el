3827bfbf9489c03aee0e321a7082c1fa085ec7e2
zhen.pei <jaypei97159@gmail.com>
2014-03-23 21:35:02 +0800

    change some keybinds, add neotree

--- emacsconf/emacs.d/templates/template.el
+++ emacsconf/emacs.d/templates/template.el
@@ -1,4 +1,28 @@
-;; filename   : `(buffer-name)`
-;; created at : `(format-time-string "%F %T")`
-;; author     : `user-full-name` <`user-mail-address`>
+;;; `(file-name-nondirectory (buffer-file-name))` --- summary
 
+;; Copyright (C) 2014 `user-full-name`
+
+;; Author: `user-full-name` <`user-mail-address`>
+;; Version: 0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+$0
+
+(provide '`(file-name-nondirectory (file-name-sans-extension (buffer-file-name)))`)
+;;; `(file-name-nondirectory (buffer-file-name))` ends here
