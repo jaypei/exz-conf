fe6898568d4377afad55f633d9af1a8131aa8b23
zhen.pei <jaypei97159@gmail.com>
2014-07-20 10:49:50 +0800

    remove el-get

--- emacsconf/emacs.d/site-lisp/el-get/recipes/keychain-environment.el
+++
@@ -1,5 +0,0 @@
-(:name keychain-environment
-       :description "Loads keychain environment variables into emacs"
-       :type github
-       :pkgname "tarsius/keychain-environment"
-       :post-init (keychain-refresh-environment))
