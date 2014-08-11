af082f93e819dc7ece15cf98603b3d93bfd9513a
jaypei <jaypei97159@gmail.com>
2014-08-11 08:34:52 +0800

    add melpa

--- emacsconf/emacs.d/etc-lisp/exz-package.el
+++ emacsconf/emacs.d/etc-lisp/exz-package.el
@@ -3,8 +3,8 @@
 
 ;;; Code:
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
-                         ;; ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 (require 'package)
 (package-initialize)
 
