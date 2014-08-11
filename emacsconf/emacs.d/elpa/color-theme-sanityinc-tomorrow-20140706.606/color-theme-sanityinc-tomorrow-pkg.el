2f75a8929407d8e93d3853b02b2a9622fc8f3af2
jaypei <jaypei97159@gmail.com>
2014-08-11 09:57:42 +0800

    upgrade

--- emacsconf/emacs.d/elpa/color-theme-sanityinc-tomorrow-20140706.606/color-theme-sanityinc-tomorrow-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "color-theme-sanityinc-tomorrow" "20140706.606" "A version of Chris Kempson's various Tomorrow themes" 'nil)
---
+++ emacsconf/emacs.d/elpa/color-theme-sanityinc-tomorrow-20140807.512/color-theme-sanityinc-tomorrow-pkg.el
@@ -0,0 +1,1 @@
+(define-package "color-theme-sanityinc-tomorrow" "20140807.512" "A version of Chris Kempson's various Tomorrow themes" 'nil)
