d1c2b63a1393d28644614d1c3f94923a7e1825e0
jaypei <jaypei97159@gmail.com>
2014-09-02 18:39:48 +0800

    add slime

---
+++ emacsconf/emacs.d/elpa/slime-20140902.2/slime-pkg.el
@@ -0,0 +1,4 @@
+(define-package "slime" "20140902.2" "Superior Lisp Interaction Mode for Emacs"
+  '((cl-lib "0.5"))
+  :url "https://github.com/slime/slime" :keywords
+  '("languages" "lisp" "slime"))
