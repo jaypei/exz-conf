c7f1e4d53e2c5745fb86aae64daebd4d9b44e14a
jaypei <jaypei97159@gmail.com>
2014-09-11 15:14:27 +0800

    add multiple-cursors

--- emacsconf/emacs.d/etc-lisp/exz-keybindings.el
+++ emacsconf/emacs.d/etc-lisp/exz-keybindings.el
@@ -67,5 +67,15 @@
 (global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 
+
+;; multiple-cursors
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-unset-key (kbd "M-<down-mouse-1>"))
+(global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
+
+
 ;;; exz-keybindings.el ends here
 (provide 'exz-keybindings)
