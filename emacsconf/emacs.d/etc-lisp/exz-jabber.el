f45f5fb51296daf96e41962e5f481041ec0997d3
jaypei <jaypei97159@gmail.com>
2016-04-19 12:31:47 +0800

    remove jabber

--- emacsconf/emacs.d/etc-lisp/exz-jabber.el
+++ emacsconf/emacs.d/etc-lisp/exz-jabber.el
@@ -22,17 +22,6 @@
 
 ;;; Code:
 
-(setq jabber-invalid-certificate-servers '("l-im1.vy.corp.qunar.com"))
-
-(setq jabber-account-list
-      '(("jaypei@l-im1.vy.corp.qunar.com"
-         (:network-server . "l-im1.s.corp.qunar.com")
-         (:port . "5222")
-         (:connection-type . starttls))))
-
-(setq jabber-muc-autojoin '("qunar群聊@conference.l-im1.vy.corp.qunar.com" "dujia-mobile@conference.l-im1.vy.corp.qunar.com"))
-(setq jabber-invalid-certificate-servers '("l-im1.vy.corp.qunar.com"))
-(setq starttls-extra-arguments  '("--insecure"))
 
 (provide 'exz-jabber)
 ;;; exz-jabber.el ends here
